0e780698f08ab99a8f6e6376518200d51292e587
suciartiara <artiaras22d@student.unhas.ac.id>
2024-12-02 21:06:23 +0800

    chore: create a base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function __construct()
+    {
+        // Any initialization needed
+    }
+
+    protected function renderInertia(string $view, array $props = [])
+    {
+        return Inertia::render($view, $props);
+    }
 }
